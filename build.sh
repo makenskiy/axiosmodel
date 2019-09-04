8bddb2a51b9d25c5281a1b9bb5161a0c013b81db
Viktor Makenskiy <makenskiy@gmail.com>
2019-09-04 11:39:19 +0200

    Add publish npm

--- build.sh
+++ build.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
-# @todo add logic for "npm publish"
-webpack --mode production
-cp dist/index.html tutorials/index.html
-cp dist/index.js tutorials/index.js
-npm run jsdoc
-# npm publish --access=public
+publish () {
+  default="no"
+  read -p "npm publish? (y/n) " answer
+  : ${answer:=$default}
+
+  if [ $answer = "y" ] || [ $answer = "Y" ] || [ $answer = "Yes" ] || [ $answer = "yes" ]; then
+    npm publish --access=public
+  fi
+}
+
+webpack --mode production && cp dist/index.html tutorials/index.html && cp dist/index.js tutorials/index.js && npm run jsdoc && publish
+
